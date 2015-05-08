1da7940ca9cf7f451a9a8d795b43477bcd108b96
Mehrdad Yazdani <myazdani@gmail.com>
2015-05-08 16:55:01 -0700

    added plotly bit

--- scripts/analysis/PCA-abundance.R
+++ scripts/analysis/PCA-abundance.R
@@ -8,4 +8,18 @@ df = read.csv("./processedData/species-data.csv", header = TRUE, stringsAsFactor
 
 df[is.na(df)] = 0
 
-df.pca = prcomp(as.matrix(df[,-1]))
+pca.result = prcomp(as.matrix(df[,-1]))
+
+pca.df = as.data.frame(pca.result$x)
+pca.df$sample.date = df[,1]
+
+library(ggplot2)
+library(GGally)
+
+#ggpairs(pca.df, columns = c(1:4), title = "uBiome Species for LS", params=list(corSize=2))
+
+ggplot(pca.df, aes(x = PC1, y = PC2, label = sample.date)) +geom_text() -> p
+
+library(plotly)
+py = plotly()
+py$ggplotly(p, kwargs=list(filename="LS-ubiome-species-PCA", fileopt="overwrite"))
