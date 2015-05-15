af05aa6936d85a4d6842a39c90ec91810d58c9dc
Mehrdad Yazdani <myazdani@gmail.com>
2015-05-15 14:25:23 -0700

    added time series plots for MDS of Bray-Curtis

--- scripts/analysis/MDS-bray-curtis.R
+++ scripts/analysis/MDS-bray-curtis.R
@@ -23,5 +23,20 @@ fit # view results
 # plot solution 
 res = data.frame(x = fit$points[,1], y = fit$points[,2], dates = target.data$ubiome.sample.date)
 library(ggplot2)
-
+library(plotly)
+py = plotly()
 ggplot(res, aes(x = x, y = y, label = dates)) + geom_text() -> p
+py$ggplotly(p, kwargs=list(filename="LS-ubiome-Bray-Curtis-MDS", fileopt="overwrite"))
+
+##-----------------------------------------------------
+## Plot time series of PC's
+##-----------------------------------------------------
+res$dates = as.Date(res$dates, format='%m.%d.%Y')
+
+library(reshape)
+res.m = melt(res, id = "dates")
+
+ggplot(res.m, aes(x = dates, y = value)) + geom_point() + 
+  facet_wrap(~variable, nrow = 3, scales = "free_y") + stat_smooth() -> p
+
+py$ggplotly(p, kwargs=list(filename="LS-ubiome-Bray-Curtis-MDS-vs-time", fileopt="overwrite"))
