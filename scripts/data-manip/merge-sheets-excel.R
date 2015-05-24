0ecb826461e775c22da81778946bd6f4060aea19
Mehrdad Yazdani <myazdani@gmail.com>
2015-05-24 14:41:22 -0700

    removed suspicious dates and changed script to order columns by date

--- scripts/data-manip/merge-sheets-excel.R
+++ scripts/data-manip/merge-sheets-excel.R
@@ -10,8 +10,14 @@ sheets = c("family", "genus", "species")
 
 library(readxl)
 library(plyr)
+
 data.path = "./data/Excel/"
 excel.files = list.files(data.path, full.names = FALSE)
+files.to.skip = c("9.25.2013 Ubiome kit 900-009-361.xls", "10.2.2013 Ubiome kit 380-002-047.xls",
+                  "10.10.2013 Ubiome kit 900-003-627.xls", "10.23.2013 Ubiome kit 900-003-628.xls",
+                  "2.17.2014 Ubiome kit 106-003-133.xls", "5.4.2014 Ubiome kit 151-003-131.xls")
+excel.files = setdiff(excel.files, files.to.skip)
+
 for (sheet in c(1:length(sheets))){
   data.files = list()
   date.ids = c()
@@ -29,6 +35,8 @@ for (sheet in c(1:length(sheets))){
   }
   
   res = join_all(data.files, type = "full", match = "all")
-  
+  date.cols = unname(sapply(names(res[,-1]), FUN = function(x) strsplit(x, split = " abundance")[[1]][1]))
+  date.cols = as.Date(date.cols, format='%m.%d.%Y')
+  res = res[,c(1,1+order(date.cols))]
   write.csv(res, file = paste0("./processedData/merged-excel-sheets/",sheets[sheet],"-data.csv"), row.names = FALSE, quote = FALSE)
 }
